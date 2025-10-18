d42cac2f4c1b46d284deffda462ff5c3a07a5396
Walid <ow_bouhenika@esi.dz>
2025-10-18 17:56:37 +0100

    filled with random data

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+# RANDOM data just to test our study case
+todolist =[
+    {
+        "IDtodo":1,"TodoText":"Study" , "Due":"Tommorow" 
+    },
+      {
+        "IDtodo":2,"TodoText":"Go to gym" , "Due":"Tommorow" 
+    },
+      {
+        "IDtodo":3,"TodoText":"Buy a new Laptop" , "Due":"Next Month" 
+    }
+]
 
 @app.get("/")
 def main():
